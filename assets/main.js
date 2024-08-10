26d486a9f5b562a7a63ad710344c2eb33c401fb7
Oleksandra1 <oleksandra.tkachenko154.com>
2024-08-10 14:33:19 +0300

    test-project

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+/*--------Slide-show-------*/
+let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let slides = document.querySelectorAll(".slides");
+        let dots = document.querySelectorAll(".dot");
+
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        slides[slideIndex - 1].style.display = "block";  
+        dots[slideIndex - 1].className += " active";
+
+        setTimeout(showSlides, 2000); // Смена изображения каждые 3 секунды
+    }
